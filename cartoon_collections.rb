aee823245e0d8ac1e1dd497a5dc27b2552e5505d
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 16:44:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(long_calls)
-  long.calls.
+  long.calls.include? |long_calls| do
+    
+    end
 end
 
 def find_the_cheese# code an argument here
