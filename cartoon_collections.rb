04fcd7e12a2312963ad69f9351d73325d1d0446b
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 16:45:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(long_calls)
-  long.calls.include? |long_calls| do
+  long.calls.include?() |long_calls| do
     
     end
 end
